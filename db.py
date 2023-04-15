2cbbc8e73dab30d13f8bd3ba369165299289aa5b
Egotblin <ikriannikove01@gmail.com>
2023-04-16 00:16:06 +0300

    пока что нет конфига

--- db.py
+++ db.py
@@ -21,7 +21,7 @@ CREATE TABLE test_table
 # Параметры для подключения к БД
 connect_params = {"user": "postgres",  # пароль, который указали при установке PostgreSQL
                   "database": "test_db",
-                  "password": "postgres",
+                  "password": "Admin",
                   "host": "localhost",
                   "port": "5432"}
 
